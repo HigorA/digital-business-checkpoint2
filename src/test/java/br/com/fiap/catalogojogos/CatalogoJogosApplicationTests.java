43f33a922d57ea51e5ed25997aa8b2f41e0d22c1
higor <higor.a.santos12@hotmail.com>
2023-04-10 13:22:35 -0300

    Primeiro commit

---
+++ src/test/java/br/com/fiap/catalogojogos/CatalogoJogosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.catalogojogos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoJogosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
