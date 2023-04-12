10b15918450849eab8f4a5c02a0463cbd3cb9cbc
higor <higor.a.santos12@hotmail.com>
2023-04-11 22:36:34 -0300

    Implementação do reporitory

---
+++ src/main/java/br/com/fiap/catalogojogos/repositories/JogoRepository.java
@@ -0,0 +1,13 @@
+package br.com.fiap.catalogojogos.repositories;
+
+import br.com.fiap.catalogojogos.model.entity.Jogo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JogoRepository extends JpaRepository<Jogo, Long> {
+
+    Page<Jogo> findAllByAtivoTrue(Pageable paginacao);
+}
